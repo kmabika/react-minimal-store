fe6bc465e499cb02c0df7afb7e56346a091aaa1e
kmabika <kmabika@users.noreply.github.com>
2022-05-31 09:56:25 +0000

    Prettified Code!

--- graphql-endpoint/README.md
+++ graphql-endpoint/README.md
@@ -12,4 +12,4 @@ In order to start this endpoint, follow these steps
 2. Build the project - `yarn build`
 3. Start the project - `yarn start`
 
->**Note**: During the assignment, do not modify this endpoint. Also, no need to provide it along with your assessment.
+> **Note**: During the assignment, do not modify this endpoint. Also, no need to provide it along with your assessment.
--- graphql-endpoint/dist/resolvers/currencies.js
+++ graphql-endpoint/dist/resolvers/currencies.js
@@ -1,8 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+var currency_converter_1 = __importDefault(require('../util/currency-converter'));
+var currencyResolver = function () {
+  return currency_converter_1.default.availableCurrencies;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var currency_converter_1 = __importDefault(require("../util/currency-converter"));
-var currencyResolver = function () { return currency_converter_1.default.availableCurrencies; };
 exports.default = currencyResolver;
